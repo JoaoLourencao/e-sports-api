6be20d3e0649a0e553aaf1f80048ed0a55aa87d2
João Lourenção <jotap.lourencao@gmail.com>
2022-09-21 15:27:56 -0300

    feat: new endpoints

---
+++ README.md
@@ -0,0 +1,27 @@
+### Backend
+
+### Game
+
+id
+title
+bannerUrl
+
+### Ad
+
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+userVoiceChannel
+createdAt
+
+## Casos de uso
+
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anúncio por game
+- Buscar dsicord pelo ID do anúncio
---
+++ src/utils/utils.ts
@@ -0,0 +1,14 @@
+export function hoursToMinutes(hourString:string) {
+  const [hours, minutes] = hourString.split(':').map(Number)
+
+  const minutesAmount = (hours * 60) + minutes
+
+  return minutesAmount
+}
+
+export function minutesToHours(minutesAmout: number) {
+  const hours = Math.floor(minutesAmout/60)
+  const minutes = minutesAmout % 60
+
+  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`
+}
